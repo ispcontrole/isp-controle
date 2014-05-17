c9caacf6081d12fe0bc8f80fbf376265e32ec3f1
Oliver Stoffel <ostoffel@users.noreply.github.com>
2014-05-17 09:16:48 +0200

    nothing
    Nothing to say about

--- isp-controle_scripts/installscripts/iredmail_table_updates_vmaildb.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `alias` ADD `guiid` INT NOT NULL , ADD INDEX ( `guiid` );
-ALTER TABLE `mailbox` ADD `guiid` INT NOT NULL , ADD INDEX ( `guiid` ); 
-ALTER TABLE `domain` ADD `guiid` INT NOT NULL , ADD INDEX ( `guiid` );
